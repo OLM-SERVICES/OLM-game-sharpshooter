85928bae258e0cee899e0adacd5b66b7893c0784
OLM-SERVICES <olmbetng@gmail.com>
2026-06-29 18:13:02 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 import Phaser from 'phaser'
 import { GameScene } from './scenes/GameScene'
 
+const container = document.getElementById('game-container')!
+
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
+  width: container.clientWidth || window.innerWidth,
+  height: container.clientHeight || window.innerHeight,
   backgroundColor: '#05001A',
   parent: 'game-container',
   scene: [GameScene],
   scale: {
     mode: Phaser.Scale.RESIZE,
     autoCenter: Phaser.Scale.CENTER_BOTH,
+    parent: 'game-container',
     width: '100%',
     height: '100%',
-  },
-  input: {
-    touch: {
-      capture: true,
-    },
   },
   render: {
     antialias: true,
@@ -25,4 +25,5 @@ const config: Phaser.Types.Core.GameConfig = {
   },
 }
 
-new Phaser.Game(config)
+const game = new Phaser.Game(config)
+game.scale.setZoom(window.devicePixelRatio || 1)
